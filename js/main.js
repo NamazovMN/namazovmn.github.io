ee2f13ce8e4e1a85e2af03f21591c7a89b7e4860
NamazovMN <mahammad.namazov@outlook.com>
2023-08-23 17:48:05 +0400

    web updated

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
-let burgerBtn = document.querySelector(".nav-phone-btn");
-let burgerMenu = document.querySelector(".nav-phone")
+let burgerBtn = document.querySelector(".nav-box-phone-btn")
+let burgerMenu = document.querySelector(".nav-phone-menu")
 
 let isBurgerOpen = false;
 
 burgerBtn.onclick = function() {
     if (!isBurgerOpen) {
-        burgerMenu.style.display = 'block';
-        burgerBtn.style.backgroundPosition = 'center left 50px, center';
+        burgerMenu.style.display = "block";
+        burgerBtn.style.backgroundPosition = "center left 50px, center";
         isBurgerOpen = true;
+
     }
-    else if (isBurgerOpen) {
-        burgerMenu.style.display = 'none';
-        burgerBtn.style.backgroundPosition = 'center, center left 50px';
+    else if (isBurgerOpen){
+        burgerMenu.style.display = "none";
+        burgerBtn.style.backgroundPosition = "center, center left 50px";
         isBurgerOpen = false;
     }
 }
