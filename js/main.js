6e7e83271ce661030c921fe2ef23aba788945989
NamazovMN <mahammad.namazov@outlook.com>
2023-08-30 20:42:29 +0400

    projects site is ready to use

--- js/main.js
+++ js/main.js
@@ -8,11 +8,25 @@ burgerBtn.onclick = function() {
         burgerMenu.style.display = "block";
         burgerBtn.style.backgroundPosition = "center left 50px, center";
         isBurgerOpen = true;
+        console.log(isBurgerOpen)
 
     }
     else if (isBurgerOpen){
         burgerMenu.style.display = "none";
         burgerBtn.style.backgroundPosition = "center, center left 50px";
         isBurgerOpen = false;
+        console.log(isBurgerOpen)
+
     }
 }
+
+let menuElements = document.querySelectorAll(".burger-element")
+console.log(menuElements)
+
+menuElements.forEach(sectionEl => {
+    sectionEl.onclick = function() {
+        burgerMenu.style.display='none';
+        burgerBtn.style.backgroundPosition = "center, center left 50px";
+        isBurgerOpen = false;
+    }
+})
